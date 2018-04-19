a182d851344dcf207e8276e319cf23ffbbdc3f5a
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-19 11:44:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,5 +43,5 @@ end
 
 =begin
 building a multidimensional monopoly hash adding key-value pairs to the third level of the monopoly hash that we continued building in the #monopoly_with_second_tier method #monopoly_with_thi
-rd_tier sets the 4th key of the :rent_in_dollars hash to a symbol, :four_pieces_owned, whose value is the integer 200
+rd_tier sets the 1st key of the :names hash to a symbol, :reading_railroad, whose value is an empty hash
 =end
